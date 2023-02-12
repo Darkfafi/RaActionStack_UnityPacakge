86b87a38f60ef966b08a7104b6dd282739dccb5f
DESKTOP-3T4A7P1\Ramses <ik_ben_ramses@hotmail.com>
2023-02-12 22:32:55 +0100

    Initial Commit

---
+++ RaAction.cs
@@ -0,0 +1,156 @@
+using RaCollection;
+using System;
+using System.Collections.Generic;
+
+namespace RaActionsStack
+{
+	public class RaAction : IDisposable
+	{
+		public delegate void Handler(RaAction action);
+		private readonly RaElementCollection<RaActionData> _data = new RaElementCollection<RaActionData>();
+		private readonly HashSet<string> _tags = new HashSet<string>();
+
+		private Handler _method;
+
+		public bool CanResolve => ActionState == State.None;
+
+		public State ActionState
+		{
+			get; private set;
+		}
+
+		public static RaAction Create(Handler method)
+		{
+			return new RaAction(method);
+		}
+
+		private RaAction(Handler method)
+		{
+			_method = method;
+		}
+
+		public void Cancel()
+		{
+			ThrowIfNotNoneState(nameof(Cancel));
+			ActionState = State.Cancelled;
+		}
+
+		public void Resolve()
+		{
+			ThrowIfNotNoneState(nameof(Resolve));
+			ActionState = State.Resolved;
+			_method?.Invoke(this);
+		}
+
+		public bool HasTag(string tag) => _tags.Contains(tag);
+
+		public bool HasData(string key) => _data.Contains(key);
+
+		public RaAction SetTag(string tag)
+		{
+			ThrowIfDisposedState(nameof(SetTag));
+			_tags.Add(tag);
+			return this;
+		}
+
+		public RaAction RemoveTag(string tag)
+		{
+			ThrowIfDisposedState(nameof(SetTag));
+			_tags.Remove(tag);
+			return this;
+		}
+
+		public RaAction RemoveData(string id)
+		{
+			ThrowIfDisposedState(nameof(RemoveData));
+			_data.Remove(id);
+			return this;
+		}
+
+		public RaAction SetData(string id, object data)
+		{
+			ThrowIfDisposedState(nameof(SetData));
+			_data.Add(RaActionData.Create(id, data));
+			return this;
+		}
+
+		public T GetData<T>()
+		{
+			TryGetData(out T data);
+			return data;
+		}
+
+		public bool TryGetData<T>(out T value)
+		{
+			if(_data.TryGetItem(out RaActionData data, (x) => x.Value is T))
+			{
+				value = (T)data.Value;
+				return true;
+			}
+
+			value = default;
+			return false;
+		}
+
+		public bool TryGetData<T>(string key, out T value)
+		{
+			if(_data.TryGetItem(key, out RaActionData data) && data.Value is T castedValue)
+			{
+				value = castedValue;
+				return true;
+			}
+			value = default;
+			return false;
+		}
+
+		public List<T> GetAllData<T>()
+		{
+			List<T> values = new List<T>();
+			_data.ForEach((data, index) =>
+			{
+				if(data.Value is T castedValue)
+				{
+					values.Add(castedValue);
+				}
+			});
+			return values;
+		}
+
+		public void Dispose()
+		{
+			if(ActionState == State.Disposed)
+			{
+				return;
+			}
+
+			_method = null;
+			_data.Clear();
+			_tags.Clear();
+			ActionState = State.Disposed;
+		}
+
+		private void ThrowIfNotNoneState(string method)
+		{
+			if(ActionState != State.None)
+			{
+				throw new InvalidOperationException($"Can't perform {method} on consumed Action [{nameof(ActionState)}: {ActionState}]");
+			}
+		}
+
+		private void ThrowIfDisposedState(string method)
+		{
+			if(ActionState == State.Disposed)
+			{
+				throw new InvalidOperationException($"Can't perform {method} on dispossed Action [{nameof(ActionState)}: {ActionState}]");
+			}
+		}
+
+		public enum State
+		{
+			None = 0,
+			Resolved = 1,
+			Cancelled = 2,
+			Disposed = 100,
+		}
+	}
+}
---
+++ RaActionStack.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RaActionsStack
+{
+	public class RaActionStack : IDisposable
+	{
+		public event RaAction.Handler ActionPushedEvent;
+		public event RaAction.Handler ActionPoppedEvent;
+		public event RaAction.Handler ActionProcessingEvent;
+		public event RaAction.Handler ActionResolvedEvent;
+		public event RaAction.Handler ActionCancelledEvent;
+		public event Action StartProcessingEvent;
+		public event Action EndProcessingEvent;
+
+		private Stack<RaAction> _stack = new Stack<RaAction>();
+		private bool _isProcessing = false;
+
+		public void Push(RaAction action)
+		{
+			_stack.Push(action);
+			ActionPushedEvent?.Invoke(action);
+			Process();
+		}
+
+		private void Process()
+		{
+			if(_isProcessing || _stack.Count == 0)
+			{
+				return;
+			}
+
+			_isProcessing = true;
+
+			StartProcessingEvent?.Invoke();
+
+			List<RaAction> processedActions = new List<RaAction>();
+
+			while(_stack.Count > 0)
+			{
+				RaAction action = _stack.Peek();
+
+				ActionProcessingEvent?.Invoke(action);
+
+				// If the peek has changed, go to new peek first
+				if(_stack.Peek() != action)
+				{
+					continue;
+				}
+
+				action = _stack.Pop();
+
+				// When stack has stabalized, Resolve unresolved actions
+				if(action.CanResolve)
+				{
+					action.Resolve();
+				}
+
+				// Send appropriate events on new Action State
+				switch(action.ActionState)
+				{
+					case RaAction.State.Resolved:
+						ActionResolvedEvent?.Invoke(action);
+						break;
+					case RaAction.State.Cancelled:
+						ActionCancelledEvent?.Invoke(action);
+						break;
+				}
+
+				ActionPoppedEvent?.Invoke(action);
+
+				processedActions.Add(action);
+			}
+
+			for(int i = processedActions.Count - 1; i >= 0; i--)
+			{
+				processedActions[i].Dispose();
+			}
+
+			_isProcessing = false;
+
+			EndProcessingEvent?.Invoke();
+		}
+
+		public void Dispose()
+		{
+			ActionPushedEvent = null;
+			ActionPoppedEvent = null;
+			ActionProcessingEvent = null;
+			ActionResolvedEvent = null;
+			ActionCancelledEvent = null;
+
+			_isProcessing = default;
+
+			while(_stack.Count > 0)
+			{
+				_stack.Pop().Dispose();
+			}
+
+			_stack = null;
+		}
+	}
+}
